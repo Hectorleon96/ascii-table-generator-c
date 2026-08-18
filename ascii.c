9ef8dc52b9d31e6150b95362e29ce303185c4456
Hectorleon96 <jobleon.hector@gmail.com>
2026-08-18 01:41:27 -0600

    calculate max length of points data

--- ascii.c
+++ ascii.c
@@ -2,11 +2,13 @@
 #include <string.h>
 
 #define DATA_LENGTH 3
+#define NAME_MAX_LENGTH 20
+#define POINTS_MAX_LENGTH 5
 
 typedef struct
 {
-  char name[20];
-  int age;
+  char name[NAME_MAX_LENGTH];
+  int points;
 } Data;
 
 int main()
@@ -15,31 +17,43 @@ int main()
   char *headers[] = {"name", "age"};
 
   Data data[DATA_LENGTH] = {
-      {"Hector", 27},
+      {"Hector", 27782},
       {"Shurumbe", 30},
-      {"Adrian", 19},
+      {"Adrian", 1299},
   };
 
   int dataLength = sizeof(data) / sizeof(data[0]);
 
-  char namesIterator[20] = "";
+  char namesIterator[NAME_MAX_LENGTH] = "";
+  int pointsIterator = 0;
 
   for (int i = 0; i < dataLength; i++)
   {
     if (strlen(data[i].name) > strlen(namesIterator))
     {
       strcpy(namesIterator, data[i].name);
-      continue;
+    }
+
+    if (data[i].points > pointsIterator)
+    {
+      pointsIterator = data[i].points;
     }
   }
 
   int namesMaxLength = strlen(namesIterator);
-  printf("%d\n", namesMaxLength);
+  char maxPointsBuffer[POINTS_MAX_LENGTH];
+  int pointsMaxLength = snprintf(maxPointsBuffer, sizeof(maxPointsBuffer), "%d", pointsIterator);
+
+  printf("Name max length: %d\n", namesMaxLength);
+  printf("Points age length: %d\n", pointsMaxLength);
 
   return 0;
 }
 
 /*
+
+Output reference:
+
 +----------+-----+-------+
 | Name     | Age | Score |
 +----------+-----+-------+
